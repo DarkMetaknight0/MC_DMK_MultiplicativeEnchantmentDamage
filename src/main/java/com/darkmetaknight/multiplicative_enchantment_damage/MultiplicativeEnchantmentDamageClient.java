694afd38dcb489a24570071b6735b87768265a5e
DarkMetaKnight <darkmetaknight@prodigy.net>
2026-05-03 14:37:16 -0500

    Updated version per mod standards

--- src/main/java/com/darkmetaknight/multiplicative_enchantment_damage/MultiplicativeEnchantmentDamageClient.java
+++ src/main/java/com/darkmetaknight/multiplicative_enchantment_damage/MultiplicativeEnchantmentDamageClient.java
@@ -1,12 +1,9 @@
 package com.darkmetaknight.multiplicative_enchantment_damage;
 
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
@@ -22,10 +19,4 @@ public class MultiplicativeEnchantmentDamageClient {
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
-    @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        MultiplicativeEnchantmentDamage.LOGGER.info("HELLO FROM CLIENT SETUP");
-        MultiplicativeEnchantmentDamage.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-    }
 }
